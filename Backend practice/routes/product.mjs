e8b6aba7c361ded33e4d447a77a33c3fe4b8a450
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-21 11:24:29 -0700

    practicee

--- Backend practice/routes/product.mjs
+++ Backend practice/routes/product.mjs
@@ -2,8 +2,12 @@ import express  from "express";
 
 const router = express.Router()
 
-const data = []
+const data = [{ "id": 1, "title": "iPhone 9", "description": "An apple mobile which is nothing like apple", "price": 549, "discountPercentage": 12.96, "rating": 4.69, "stock": 94, "brand": "Apple", "category": "smartphones", "thumbnail": "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg", "images": ["https://cdn.dummyjson.com/product-images/1/1.jpg", "https://cdn.dummyjson.com/product-images/1/2.jpg", "https://cdn.dummyjson.com/product-images/1/3.jpg", "https://cdn.dummyjson.com/product-images/1/4.jpg", "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"] }]
 
+// get all product in api
 
+router.get('/' , (req , res) => {
+    res.send({ massasge : 'Single Product fetched successfully', data })
+})
 
 export default router
