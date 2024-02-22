26e4317afb98af7c85afd16018624d32392a1fb6
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-21 23:53:45 -0700

    working

--- Backend practice/index.mjs
+++ Backend practice/index.mjs
@@ -1,13 +1,13 @@
 import  express  from "express";
-import routes from './routes'
+import routes from './routes/index.mjs'
 
 const app = express()
 
 // console is code se chalta rhe ga
-app.use('/', routes)
 
-app.listen( 3001 , function () {
+app.listen( 3001 , () => {
     console.log('Server is running at port 3001');
 })
 
 // Use method Post, Put, Get, Delete sb kelie use hota ha
+app.use('/', routes);
