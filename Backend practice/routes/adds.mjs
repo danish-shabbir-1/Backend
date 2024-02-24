14adf481c19c37f76e3f5c563b7dedb50262b477
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-24 06:40:31 -0700

    working pending error onn fetchinf

--- Backend practice/routes/adds.mjs
+++ Backend practice/routes/adds.mjs
@@ -1,16 +1,15 @@
 import express from "express";
-import addas from "../models/Adds.mjs";
+import adds from "../models/Adds.mjs";
 
 const router = express.Router();
 
 router.post("/post", async (req, res) => {
   try {
-    const addItemInDb = await addas(req.body);
-    addItemInDb.save();
-console.log(req.body);
+    const addItemInDb = await adds(req.body);
+    addItemInDb.save()
     res.send({ massage: "Product Added Succesfully" });
   } catch (error) {
-    res.send({ massage : e.massage});
+    res.send({ massage: e.massage });
   }
 });
 
