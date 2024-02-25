b86df7045f7456bac4b8f5a0c9130555e112d865
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-25 12:00:20 -0700

    All app working done and save method okay in db

--- Backend practice/config/db.mjs
+++ Backend practice/config/db.mjs
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb+srv://Danish:Danish<1111>@cluster0.kpz22ga.mongodb.net');
+mongoose.connect('mongodb+srv://Danish:Danish<1111>@cluster0.kpz22ga.mongodb.net/OlX');
 
-export default mongoose 
+export default mongoose;
