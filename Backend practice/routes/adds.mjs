833369201ef4d6893b59cf9911c1c764a3ca9381
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-25 12:14:05 -0700

    Item get in database work complete

--- Backend practice/routes/adds.mjs
+++ Backend practice/routes/adds.mjs
@@ -2,6 +2,14 @@ import express from 'express'
 import Adds from '../models/Adds.mjs'
 const router = express.Router()
 
+// Get data in db
+
+router.get('/' , async (req , res) => {
+    const getDataFromDb = await Adds.find()
+    res.send({massage : 'Data fetched successfully' , data : getDataFromDb});
+})
+
+// Add data in db 
 
 router.post("/post", async (req, res) => {
     try{
