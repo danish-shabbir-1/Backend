8feee8a55210904711a4df62aa10a7c5d8e922bb
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-28 00:50:19 -0700

    working

--- Backend practice/routes/adds.mjs
+++ Backend practice/routes/adds.mjs
@@ -9,6 +9,24 @@ router.get('/' , async (req , res) => {
     res.send({massage : 'Data fetched successfully' , data : getDataFromDb});
 })
 
+router.get('/:search' , async (req , res) => {
+
+  console.log(req.params);
+    const getDataFromDb = await Adds.find({Title : req.params.search})
+
+    res.send({massage : 'Data fetched successfully', getDataFromDb }) 
+})
+
+router.delete('/:id' , async (req , res) => {
+    const getDataFromDb = await Adds.findByIdAndDelete(req.params.id)
+    res.send({massage : 'Data Deleted successfully'});
+})
+
+router.put  ('/:id' , async (req , res) => {
+    const getDataFromDb = await Adds.findByIdAndUpdate(req.params.id, req.body)
+    res.send({massage : 'Data Updated successfully'});
+})
+
 // Add data in db 
 
 router.post("/post", async (req, res) => {
