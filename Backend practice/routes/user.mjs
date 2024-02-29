98f7ef6cd4e0dfa3ac19f4d534b42e08779fe370
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-29 05:23:44 -0700

    working

--- Backend practice/routes/user.mjs
+++ Backend practice/routes/user.mjs
@@ -1,10 +1,14 @@
 import express from "express";
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    console.log(req.body);
+    res.send({massage : 'check Succesfull'})
+})
+
 router.post('/register', (req, res) => {
     console.log(req.body);
     res.send({massage : 'User Register Succesfull'})
 })
-
 
 export default router
