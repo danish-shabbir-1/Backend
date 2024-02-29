7de8c382ce32809c3f9fae94d9fcb77d809c814c
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-29 09:19:59 -0700

    add data done in db

--- Backend practice/routes/user.mjs
+++ Backend practice/routes/user.mjs
@@ -1,14 +1,23 @@
 import express from "express";
+import User from '../models/User.mjs'
 const router = express.Router()
+
 
 router.get('/', (req, res) => {
     console.log(req.body);
     res.send({massage : 'check Succesfull'})
 })
 
-router.post('/register', (req, res) => {
-    console.log(req.body);
-    res.send({massage : 'User Register Succesfull'})
+router.post('/register', async (req, res) => {
+    try {
+        let userInfo = new User(req.body)
+        console.log(userInfo);
+        await userInfo.save();
+        res.send({massage : 'User Register Succesfull'})
+    } catch (e) {
+        console.log(e.massage);
+    }
+
 })
 
 export default router
