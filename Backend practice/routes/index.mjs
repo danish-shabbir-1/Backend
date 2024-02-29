2ea96014a6a084d89758dda4f668b1bcb67d7d16
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-28 23:13:27 -0700

    working on register

--- Backend practice/routes/index.mjs
+++ Backend practice/routes/index.mjs
@@ -1,10 +1,12 @@
 import express  from "express";
 import product from './product.mjs'
 import adds from './adds.mjs'
+import user from './user.mjs' 
 
 const router = express.Router()
 
 router.use('/product' , product)
 router.use('/ads' , adds)
+router.use('/user' , user)
 
 export default router
