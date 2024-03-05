d9098af6bc1438027a654deaac60d20cc607cdbf
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-03-05 04:35:18 -0700

    practice

--- Backend practice/models/userInfo.mjs
+++ Backend practice/models/userInfo.mjs
@@ -1,15 +1,18 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcryptjs'
 const { Schema } = mongoose;
 
 
 const registerSchema = new Schema({
     email : {
-        type : String,
-        required : true,
+        type: String,
+        required: true,
+        unique: true
     },
     pass  : {
         type : String,
         required : true,
+        minLength: 6
     },
     fullname : {
         type : String,
