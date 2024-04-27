61791d0ceebd28a43149e68282f485e5a236f6e1
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-04-27 14:05:58 -0700

    working logout api and jwt

---
+++ Backend/middleware/verifyToken.mjs
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken'
+import jwtSecret from '../config/jwt.mjs'
+
+
+async function verifyToken(req,res, next) {
+    
+    const token = req.headers.authorization?.slice(7);
+
+    if (!token) {
+        return res.status(401).send({ message: "No access!" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, jwtSecret);
+
+        // Check if the token's signature is valid but not necessarily if it exists in the database
+        req.userId = decoded._id;
+        req.tokenToRemove = token;
+
+        next();
+    } catch (e) {
+        // Handle invalid token or token expiration
+        return res.status(401).send({ message: "Invalid token!" });
+    }
+}
+
+export default verifyToken;
